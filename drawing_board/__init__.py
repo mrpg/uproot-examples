5e93445dd4e62e5e027586e8b6975478330c42eb
Max R. P. Grossmann <m@max.pm>
2025-06-14 22:21:41 +0200

    Update apps' license headers and DESCRIPTION

--- drawing_board/__init__.py
+++ drawing_board/__init__.py
@@ -1,8 +1,22 @@
+"""
+Docs are available at https://uproot.science/
+Examples are available at https://github.com/mrpg/uproot-examples
+
+This example app is under the 0BSD license. You can use it freely and build on it
+without any limitations and without any attribution. However, these two lines must be
+preserved in any uproot app (the license file is automatically installed in a project):
+
+Third-party dependencies:
+- uproot: LGPL v3+, see ../uproot_license.txt
+"""
+
 import colorsys
 import random
 
 from uproot.smithereens import *
 from uproot.types import Page
+
+DESCRIPTION = "An interactive drawing board"
 
 
 def generate_visible_hex():
--- focal_point/__init__.py
+++ focal_point/__init__.py
@@ -1,5 +1,19 @@
+"""
+Docs are available at https://uproot.science/
+Examples are available at https://github.com/mrpg/uproot-examples
+
+This example app is under the 0BSD license. You can use it freely and build on it
+without any limitations and without any attribution. However, these two lines must be
+preserved in any uproot app (the license file is automatically installed in a project):
+
+Third-party dependencies:
+- uproot: LGPL v3+, see ../uproot_license.txt
+"""
+
 from uproot.fields import *
 from uproot.smithereens import *
+
+DESCRIPTION = "Coordination through focal points"
 
 
 def new_player(player):
