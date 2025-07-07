76ba9d3d1d267a3af90dfd89d1b2fb631635b919
Max R. P. Grossmann <m@max.pm>
2025-07-07 14:06:24 +0200

    Add chat app

---
+++ chat/README.md
@@ -0,0 +1,7 @@
+# chat
+
+Load this config using
+
+```python
+load_config(uproot_server, config="my_config", apps=["chat"])
+```
---
+++ chat/__init__.py
@@ -0,0 +1,33 @@
+# Docs are available at https://uproot.science/
+# Examples are available at https://github.com/mrpg/uproot-examples
+#
+# This example app is under the 0BSD license. You can use it freely and build on it
+# without any limitations and without any attribution. However, these two lines must be
+# preserved in any uproot app (the license file is automatically installed in projects):
+#
+# Third-party dependencies:
+# - uproot: LGPL v3+, see ../uproot_license.txt
+
+from uproot.fields import *
+from uproot.smithereens import *
+
+
+def new_session(session):
+    session.chat = chat.create(session)
+
+
+def new_player(player):
+    chat.add_player(
+        player.session.chat,
+        player,
+        pseudonym=f"Player {player.id}",
+    )  # pseudonym is optional
+
+
+class ChatHere(Page):
+    pass
+
+
+page_order = [
+    ChatHere,
+]
