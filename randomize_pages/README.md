cd4dbd3c5a45bd3134d050acbfca30770374135b
Max R. P. Grossmann <m@max.pm>
2025-07-07 14:12:33 +0200

    Add randomize_pages app

---
+++ randomize_pages/README.md
@@ -0,0 +1,7 @@
+# randomize\_pages
+
+Load this config using
+
+```python
+load_config(uproot_server, config="my_config", apps=["randomize_pages"])
+```
---
+++ randomize_pages/__init__.py
@@ -0,0 +1,46 @@
+# Docs are available at https://uproot.science/
+# Examples are available at https://github.com/mrpg/uproot-examples
+#
+# This example app is under the 0BSD license. You can use it freely and build on it
+# without any limitations and without any attribution. However, these two lines must be
+# preserved in any uproot app (the license file is automatically installed in projects):
+#
+# Third-party dependencies:
+# - uproot: LGPL v3+, see ../uproot_license.txt
+
+
+from uproot.fields import *
+from uproot.smithereens import *
+
+DESCRIPTION = "Randomize pages"
+
+
+class A(Page):
+    pass
+
+
+class B(Page):
+    pass
+
+
+class C(Page):
+    pass
+
+
+class D(Page):
+    pass
+
+
+class E(Page):
+    pass
+
+
+page_order = [
+    Random(
+        A,
+        B,
+        C,
+        D,
+        E,
+    ),
+]
