0aea67725b0896f635ce6ae317afb9a43c8f114c
Max R. P. Grossmann <m@max.pm>
2025-09-06 10:07:10 +1000

    Add randomize_pages_allow_back

---
+++ randomize_pages_allow_back/README.md
@@ -0,0 +1,9 @@
+# randomize\_pages\_allow\_back
+
+Load this config using
+
+```python
+load_config(uproot_server, config="my_config", apps=["randomize_pages_allow_back"])
+```
+
+*Note*: This app, in contrast to `randomize\_pages`, allows players to go back to previous pages.
---
+++ randomize_pages_allow_back/__init__.py
@@ -0,0 +1,46 @@
+# Docs are available at https://uproot.science/
+# Examples are available at https://github.com/mrpg/uproot-examples
+#
+# This example app is under the 0BSD license. You can use it freely and build on it
+# without any limitations and without any attribution. However, these two lines must be
+# preserved in any uproot app (the license file is automatically installed in projects):
+#
+# Third-party dependencies:
+# - uproot: LGPL v3+, see ../uproot_license.txt
+
+
+from uproot.fields import *
+from uproot.smithereens import *
+
+DESCRIPTION = "Randomize pages (and allow players to go back)"
+
+
+class A(Page):
+    allow_back = True
+
+
+class B(Page):
+    allow_back = True
+
+
+class C(Page):
+    allow_back = True
+
+
+class D(Page):
+    allow_back = True
+
+
+class E(Page):
+    allow_back = True
+
+
+page_order = [
+    Random(
+        A,
+        B,
+        C,
+        D,
+        E,
+    ),
+]
