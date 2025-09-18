b5af50657660f4bfa3cb6b6badec86cdf02e9739
Max R. P. Grossmann <m@max.pm>
2025-09-18 19:15:24 +1000

    Add 'notifications' app

---
+++ _internal/notifications/README.md
@@ -0,0 +1,7 @@
+# notifications
+
+Load this config using
+
+```python
+load_config(uproot_server, config="my_config", apps=["notifications"])
+```
---
+++ _internal/notifications/__init__.py
@@ -0,0 +1,42 @@
+# Docs are available at https://uproot.science/
+# Examples are available at https://github.com/mrpg/uproot-examples
+#
+# This example app is under the 0BSD license. You can use it freely and build on it
+# without any limitations and without any attribution. However, these two lines must be
+# preserved in any uproot app (the license file is automatically installed in projects):
+#
+# Third-party dependencies:
+# - uproot: LGPL v3+, see ../uproot_license.txt
+
+from uproot.smithereens import *
+from uproot.types import Page
+
+DESCRIPTION = "Test smithereens.notify"
+
+
+class SendAndReceive(Page):
+    allow_back = True
+
+    @live
+    async def notify_(page, player, data: str, where_: str | None):
+        if where_ is None:
+            where = None
+        elif where_ == "...":
+            where = ...
+        else:
+            where = int(where_)
+
+        notify(
+            player,
+            players(player.session),
+            data,
+            event="Notified",
+            where=where,
+        )
+
+
+page_order = [
+    SendAndReceive,
+    SendAndReceive,
+    SendAndReceive,
+]
