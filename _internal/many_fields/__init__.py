188d5b43e2e1bd1ecf3b9e19f1ba159f33a86628
Max R. P. Grossmann <m@max.pm>
2025-09-27 19:03:58 +1000

    many_fields: Add internal example

---
+++ _internal/many_fields/__init__.py
@@ -0,0 +1,25 @@
+# Docs are available at https://uproot.science/
+# Examples are available at https://github.com/mrpg/uproot-examples
+#
+# This example app is under the 0BSD license. You can use it freely and build on it
+# without any limitations and without any attribution. However, these two lines must be
+# preserved in any uproot app (the license file is automatically installed in projects):
+#
+# Third-party dependencies:
+# - uproot: LGPL v3+, see ../uproot_license.txt
+
+from uproot.fields import *
+from uproot.smithereens import *
+
+
+DESCRIPTION = "Insert many fields"
+LANDING_PAGE = False
+
+
+def new_player(player):
+    for i in range(50):
+        for j in range(50):
+            setattr(player, f"field{i}", j)
+
+
+page_order = []
