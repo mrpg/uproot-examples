cdaaa6b97cb892e964aa22fb058c8205f80e0191
Max R. P. Grossmann <m@max.pm>
2026-01-25 15:23:21 +1100

    counter_alpine: Add example

---
+++ _internal/counter_alpine/README.md
@@ -0,0 +1,7 @@
+# counter\_alpine
+
+Load this app using
+
+```python
+load_config(uproot_server, config="counter_alpine", apps=["counter_alpine"])
+```
---
+++ _internal/counter_alpine/__init__.py
@@ -0,0 +1,37 @@
+# Docs are available at https://uproot.science/
+# Examples are available at https://github.com/mrpg/uproot-examples
+#
+# This example app is under the 0BSD license. You can use it freely and build on it
+# without any limitations and without any attribution. However, these two lines must be
+# preserved in any uproot app (the license file is automatically installed in projects):
+#
+# Third-party dependencies:
+# - uproot: LGPL v3+, see ../uproot_license.txt
+
+from uproot.smithereens import *
+
+DESCRIPTION = "Counter (uses Alpine.js on frontend)"
+
+
+def new_player(player):
+    player.counter = 0
+
+
+class Counter(Page):
+    @live
+    async def get(page, player):
+        return player.counter
+
+    @live
+    async def increment(page, player):
+        player.counter += 1
+        return player.counter
+
+    @live
+    async def reset(page, player):
+        player.counter = 0
+
+
+page_order = [
+    Counter,
+]
