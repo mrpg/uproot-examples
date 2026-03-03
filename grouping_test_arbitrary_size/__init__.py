5d4cbdc3420177059c22b0b47b43cad6f1b20e56
Max R. P. Grossmann <m@max.pm>
2026-03-03 22:01:38 +1100

    Use templatevars instead of context

--- grouping_test_arbitrary_size/__init__.py
+++ grouping_test_arbitrary_size/__init__.py
@@ -123,7 +123,7 @@ class ShowGroup(Page):
         player.group.dropped_out = True
 
     @classmethod
-    def context(page, player):
+    def templatevars(page, player):
         num_groups = 0
         if player.group:
             group_members = sorted(players(player.group), key=lambda p: p.name)
