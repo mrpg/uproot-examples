f6876c71396a605f6f5442a7a02a8e1bea832c77
Max R. P. Grossmann <m@max.pm>
2026-05-07 16:37:42 +1000

    prisoners_dilemma_repeated: Beautify code

--- prisoners_dilemma_repeated/__init__.py
+++ prisoners_dilemma_repeated/__init__.py
@@ -119,18 +119,13 @@ def pipeline(session):
 
 
 def played_rounds(*players):
-    # TODO: This code seems kinda convoluted and inelegant?
     return sorted(
-        {round_num for player in players for round_num in player_rounds(player)}
+        {
+            round_num
+            for player in players
+            for round_num, _ in player.within(app=APP_NAME).along("round")
+        }
     )
-
-
-def player_rounds(player):
-    return [
-        round_num
-        for round_num in range(1, C.ROUNDS + 1)
-        if player.within(app=APP_NAME, round=round_num).get("round") is not None
-    ]
 
 
 def prisoners_dilemma_groups(session):
